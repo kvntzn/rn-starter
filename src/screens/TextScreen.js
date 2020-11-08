bec53ebe8e01f0171bd01b83299c5fb306ca8227
heathkev <kevintuazon45@gmail.com>
2020-11-09 00:59:43 +0800

    Implemented text screen with states

---
+++ src/screens/TextScreen.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, TextInput } from 'react-native';
+
+const TextScreen = () => {
+    const [name, setName] = useState('');
+    const [password, setPassword] = useState('');
+
+    return (
+        <View>
+            <Text>Enter name:</Text>
+            <TextInput
+                style={styles.input}
+                autoCapitalize="none"
+                autoCorrect={false}
+                value={name}
+                onChangeText={(newValue) => setName(newValue)} />
+            <Text>My name is {name}</Text>
+
+            <Text>Enter Password:</Text>
+            <TextInput
+                style={styles.input}
+                autoCapitalize="none"
+                autoCorrect={false}
+                onChangeText={(newValue) => setPassword(newValue)} />
+            {password.length < 5 ? <Text>Password must be longer than 5 characters</Text> : null}
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    input: {
+        margin: 15,
+        borderColor: 'black',
+        borderWidth: 1
+    }
+});
+
+export default TextScreen;
